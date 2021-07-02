0f02a446fccbd6437cdabce92a6104f75f270a62
kinyeramos2021 <kinyeramo@gmail.com>
2021-07-02 16:31:50 +0300

    first commit

---
+++ src/components/header-bar-links.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="navbar-menu">
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a
+            :href="github"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-github fa-2x" aria-hidden="true"></i>
+          </a>
+          <a
+            :href="twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i class="fab fa-twitter fa-2x" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeaderBarLink",
+ data() {
+   return {
+     github: 'https://github.com/kinyeramos2021',
+     twitter: 'https://twitter.com/AmosKinyera'
+   }
+ },
+};
+</script>
